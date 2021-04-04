a827bf14ea57f2199b6996f4ddbb065f1ae97b45
JuYeong <happyu9986@gmail.com>
2021-04-04 22:06:56 +0900

    3주차과제

--- 3-week/calculater.py
+++ 3-week/calculater.py
@@ -1,23 +1,25 @@
 price=int(input("물건 값: "))
 get_money=int(input("받은 돈: "))
-give_money=price-get_money
+give_money=get_money-price
 
 print("물건 값:", price, "원")
 print("받은 돈:", get_money, "원")
 print("거스름돈:", give_money, "원")
 
-if get_money<=10000:
-    while give_money>0:
-        money_1000=give_money//1000
-        give_money%=1000 
-        money_500=give_money//500 
-        give_money%=500  
-        money_100=give_money//100
-        give_money%=100 
-        money_50=give_money//50 
-        give_money%=50 
-        money_10=give_money//10
-        give_money%=10
+money_1000=give_money //1000
+give_money%=1000 
+
+money_500=give_money //500 
+give_money%=500  
+
+money_100=give_money //100
+give_money%=100 
+
+money_50=give_money //50 
+give_money%=50 
+
+money_10=give_money //10
+give_money%=10
 
 print("1000원: ", money_1000, "개")
 print("500원: ", money_500, "개")
