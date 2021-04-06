5e742f28ec2218bd24b518cb98ebd911558c24a1
JuYeong <happyu9986@gmail.com>
2021-04-06 22:04:11 +0900

    4주차

---
+++ 4-week/report.py
@@ -0,0 +1,7 @@
+def get_description():
+    from random import choice
+    possibilities = ['rain', 'snow', 'sleet']
+    return choice[possibilities]
+
+def get_rainy():
+    return 'rain'
---
+++ 4-week/weather.py
@@ -0,0 +1,13 @@
+# import report as wl
+
+# description=wl.get_description()
+# print("Today's weather: ", description)
+
+# test=wl.get_rainy()
+# print("Today's weather: ", test)
+
+from report import get_description
+
+description=get_descriptions()
+print("Today's weather: ", description)
+
