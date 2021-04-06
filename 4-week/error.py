3f616a717584316a34cc48b2d4f706b7b32051d3
JuYeong <happyu9986@gmail.com>
2021-04-06 22:12:16 +0900

    4주차

---
+++ 4-week/error.py
@@ -0,0 +1,10 @@
+try: 
+    a=1/0
+
+except ZeroDivisionError:
+    a="누가 나누기 할 때 0으로 두냐 멍청아!"
+    
+except:
+    a="너 뭔가 에러나 ㅎㅎ"
+
+print(a)
--- 4-week/weather.py
+++ 4-week/weather.py
@@ -8,6 +8,6 @@
 
 from report import get_description
 
-description=get_descriptions()
+description=get_description()
 print("Today's weather: ", description)
 
