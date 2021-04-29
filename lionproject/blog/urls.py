5c50c0de009a4afe988ea2566b2d0debf14bb34f
JuYeong <happyu9986@gmail.com>
2021-04-30 02:18:47 +0900

    6주차과제

--- lionproject/blog/urls.py
+++ lionproject/blog/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('posts/', views.post_list, name='post_list'),
-]                 # 함수호출
+    path('posts/<int:post_id>/', views.post_detail, name='post_detail'),
+    path('posts/new/', views.post_new, name='post_new'),
+    path('posts/<int:post_id>/edit/', views.post_edit, name='post_edit'),
+    path('posts/<int:post_id>/delete/', views.post_delete, name='post_delete'),
+]                 # post_id라고 했으면 views.py 함수에도 같은 파라미터 이름 이여야함
--- lionproject/blog/views.py
+++ lionproject/blog/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from .forms import PostForm
 from .models import Post
-
 
 def post_list(request):
     post_list = Post.objects.all()  #Post클래스에서 데이터베이스에서 데이터를 가져옴
@@ -10,3 +10,54 @@ def post_list(request):
     return render(request,'blog/post_list.html', context)
 #key를 통해 value를 가져오니까
 #context['key']랑 같은거임
+
+  #첫번재 인자는 무조건 repuest
+def post_detail(request, post_id):  
+
+    post = Post.objects.get(id=post_id)
+    context = {
+        'post_id':post_id,
+        'post':post
+    }
+    return render(request,'blog/post_detail.html', context)
+
+def post_new(request):
+    if request.method == 'GET':
+        form = PostForm()
+
+    elif request.method =='POST':
+        #사용자가 입력한 데이터를 저장하는 부분
+        form = PostForm(request.POST)
+        if form.is_valid():  #사용자가 입력한 데이터에 문제가 없으면 if문 실행
+            post = form.save()
+            return redirect('post_detail', post_id=post.id)
+
+    return render(request, 'blog/post_new.html', {
+        'form': form,
+    }) 
+
+
+def post_edit(request, post_id):
+
+    post = Post.objects.get(id=post_id)
+    # post = get_object_or_404(Post, id=post_id)
+
+    if request.method == 'GET':
+        form = PostForm(instance=post)
+
+    elif request.method =='POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save()
+            return redirect('post_detail', post_id=post.id)
+            
+    return render(request, 'blog/post_edit.html', {
+        'form': form,
+        'post': post,
+    })
+
+def post_delete(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    post.delete()
+    return redirect('post_list')
+    
