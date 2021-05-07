e3bc642e7cc49afbb51ee050d6b5c5df63da7102
JuYeong <happyu9986@gmail.com>
2021-05-07 13:52:53 +0900

    7주차과제

---
+++ blogproject/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('posts/', views.post_list, name="post_list"),
+    path('posts/<int:post_id>/', views.post_detail, name="post_detail"),
+    path('posts/<int:post_id>/delete/', views.post_delete, name="post_delete"),
+    path('posts/edit/<int:post_id>/', views.post_edit, name="post_edit"),
+    path('posts/new/', views.post_new, name="post_new"),
+    path('posts/<int:post_id>/init', views.post_init, name="post_init"),
+]
