8b81e923a2fefb3d4c484058bbee6f7299651277
JuYeong <happyu9986@gmail.com>
2021-06-24 22:17:08 +0900

    init

--- team_project/web/urls.py
+++ team_project/web/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('web/', views.main, name="main"),
+    path('main/', views.main, name="main"),
+    path('login/', views.login, name="login"),
+    path('signup/', views.signup, name="signup"),
+    path('alarm/', views.alarm, name="alarm"),
 ]
